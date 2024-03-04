7a22ed38854fa97c317c69f10e576a3a7d3a59e9
NadiasCodes <De.nadia@outlook.com>
2024-03-04 12:53:24 +0100

    made a simple hero with inputs data, using useStates and onChange events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [origin, setOrigin] = useState("");
+  const [displayCharacter, setdisplayCharacter] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Build a hero</h1>
+      <div>
+        <label>Name: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Height: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setHeight(event.target.value);
+          }}
+        />
+        <label>Origin: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setOrigin(event.target.value);
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          setdisplayCharacter(true);
+        }}
+      >
+        Display Data
+      </button>
+      <div>
+        <h1>Data:</h1>
+        {displayCharacter && (
+          <ul>
+            <li>Name: {name}</li>
+            <li>Age: {age}</li>
+            <li>Height: {height}</li>
+            <li>Origin: {origin}</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
